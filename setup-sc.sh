0c786c9fa699a31ba6a14d492a40c8d15960d709
dagint <dagint@gmail.com>
2018-05-08 11:58:29 -0400

    fail2ban updates to jail.local

--- setup-sc.sh
+++ setup-sc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+intbantime=86400
+#update apt repo
 apt-get update
-apt-get install fail2ban ufw
+#install fail2ban and ufw if not present
+apt-get install fail2ban ufw -y
 
+#Configure ufw firewall
 ufw status verbose
 ufw default allow outgoing
 ufw default deny incoming
@@ -11,3 +15,9 @@ ufw allow http
 ufw allow https
 ufw allow 8042/tcp
 ufw enable
+
+#Copy jail.conf to jail.local
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sed -i 's/^bantime .*/bantime = $intbantime/g' /etc/fail2ban/jail.local
+
+
